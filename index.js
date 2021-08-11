7aca421e8c59670d2826b8c6bb0bb9af848622c1
johnnythrash <chris.garofolo@gmail.com>
2021-08-10 21:55:59 -0400

    change port settings

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-const PORT = config.PORT || 3003
+const PORT = process.env.PORT || 3003
 server.listen(PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
